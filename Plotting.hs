85b9c6d0cdd6cd7d007f3aec4d19af4069e29ed0
Sam Derbyshire <sam.derbyshire@gmail.com>
2012-01-05 05:08:54 +0100

    more cleanup

--- Plotting.hs
+++ Plotting.hs
@@ -132,7 +132,7 @@ instance Plot (IFSPlot a) where
 toCoords :: [Root] -> Resolution -> Center -> Width -> [Pixel]
 toCoords roots (rx,ry) c w  = map((\z->(floor(realPart z),ry-floor(imagPart z)))
                                   .(\z -> (rx'/w :+ 0) * (z-p)))
-                              filter (\z -> elemI z (p,p')) roots
+                              $ filter (\z -> elemI z (p,p')) roots
                             where [rx',ry'] = map fromIntegral [rx,ry]
                                   h = w * ry'/rx' 
                                   p = c - ( w/2 :+ h/2)
